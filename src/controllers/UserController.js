b28d099c41b901085382be36a8dd53e8bfb444a3
AndrewDeras <andrewderas7@gmail.com>
2023-05-15 16:07:28 -0300

    user controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,9 +5,82 @@ class UserController {
     try {
       const novoUser = await User.create(req.body);
 
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  // index
+
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // show
+
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ['Id não enviado'] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ['Usuário não existe.'] });
+      }
+
+      const updatedUser = await user.update(req.body);
+
+      return res.json(updatedUser);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((error) => error.message),
+      });
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({ errors: ['Id não enviado'] });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({ errors: ['Usuário não existe.'] });
+      }
+
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((error) => error.message),
       });
     }

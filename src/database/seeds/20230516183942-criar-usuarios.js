1a4239ab04eb8314e4c7515025dc6e148e79ece7
AndrewDeras <andrewderas7@gmail.com>
2023-05-16 16:04:49 -0300

    seeds

---
+++ src/database/seeds/20230516183942-criar-usuarios.js
@@ -0,0 +1,42 @@
+/** @type {import('sequelize-cli').Migration} */
+
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'Toni Kroos',
+          email: 'kroos@email.com',
+          password_hash: await bcryptjs.hash('88888888', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+
+        },
+        {
+          nome: 'Luka Modric',
+          email: 'modric@email.com',
+          password_hash: await bcryptjs.hash('19191919', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+
+        },
+        {
+          nome: 'Edu Camavinga',
+          email: 'camavinga@email.com',
+          password_hash: await bcryptjs.hash('12121212', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+
+        },
+      ],
+
+      {},
+    );
+  },
+
+  async down() {
+  },
+};
